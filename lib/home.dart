74079020330c017beb359c0f36b3fa0a41b24f52
Shahzaib <shahzaibrizwan7@gmail.com>
2022-10-27 11:05:07 +0500

    calculator

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 import 'package:shahz/components/my_button.dart';
 
 //import 'package:shahz/const.dart';
@@ -11,28 +12,34 @@ class HomeScr extends StatefulWidget {
 }
 
 class _HomeScrState extends State<HomeScr> {
-  var UserInput = '7';
+  var UserInput = '';
   var answer = '';
   @override
   Widget build(BuildContext context) {
     print('rebuild');
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10),
           child: Column(
+           
             children: [
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(vertical: 20),
                   child: Column(
+                     mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text(
-                        UserInput.toString(),
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
+                      Align(alignment: Alignment.bottomRight,
+                        child: Text(
+                          UserInput.toString(),
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                      ),SizedBox(height: 15,),
                       Text(answer.toString(),
-                          style: TextStyle(fontSize: 20, color: Colors.white))
+                          style: TextStyle(fontSize: 20, color: Colors.black))
                     ],
                   ),
                 ),
@@ -44,16 +51,31 @@ class _HomeScrState extends State<HomeScr> {
                     children: [
                       MYButton(
                         title: 'AC',
-                        onPress: () {},
+                        onPress: () {
+                          UserInput = '';
+                          answer = '';
+                          setState(() {});
+                        },
                       ),
                       MYButton(
                         title: '+/-',
-                        onPress: () {},
+                        onPress: () {
+                          UserInput += '+/-';
+                          setState(() {});
+                        },
                       ),
-                      MYButton(title: '%', onPress: () {}),
                       MYButton(
-                        onPress: () {},
+                          title: '%',
+                          onPress: () {
+                            UserInput += '%';
+                            setState(() {});
+                          }),
+                      MYButton(
                         title: '/',
+                        onPress: () {
+                          UserInput += '/';
+                          setState(() {});
+                        },
                         color: const Color.fromARGB(255, 1, 56, 153),
                       ),
                     ],
@@ -63,20 +85,29 @@ class _HomeScrState extends State<HomeScr> {
                       MYButton(
                         title: '7',
                         onPress: () {
-                          UserInput = '7';
+                          UserInput += '7';
                           setState(() {});
                         },
                       ),
                       MYButton(
                           title: '8',
                           onPress: () {
-                            UserInput = '8';
+                            UserInput += '8';
                             setState(() {});
                           }),
-                      MYButton(title: '0', onPress: () {}),
                       MYButton(
-                        title: 'X',
-                        onPress: () {},
+                          title: '0',
+                          onPress: () {
+                            UserInput += '0';
+                            setState(() {});
+                          }),
+                      MYButton(
+                        title: 'x',
+                        onPress: () {
+                          setState(() {
+                            UserInput += 'x';
+                          });
+                        },
                         color: const Color.fromARGB(255, 1, 56, 153),
                       ),
                     ],
@@ -85,13 +116,29 @@ class _HomeScrState extends State<HomeScr> {
                     children: [
                       MYButton(
                         title: '4',
-                        onPress: () {},
+                        onPress: () {
+                          UserInput += '4';
+                          setState(() {});
+                        },
                       ),
-                      MYButton(title: '5', onPress: () {}),
-                      MYButton(title: '6', onPress: () {}),
+                      MYButton(
+                          title: '5',
+                          onPress: () {
+                            UserInput += '5';
+                            setState(() {});
+                          }),
+                      MYButton(
+                          title: '6',
+                          onPress: () {
+                            UserInput += '6';
+                            setState(() {});
+                          }),
                       MYButton(
                         title: '-',
-                        onPress: () {},
+                        onPress: () {
+                          UserInput += '-';
+                          setState(() {});
+                        },
                         color: const Color.fromARGB(255, 1, 56, 153),
                       ),
                     ],
@@ -100,13 +147,30 @@ class _HomeScrState extends State<HomeScr> {
                     children: [
                       MYButton(
                         title: '1',
-                        onPress: () {},
+                        onPress: () {
+                          UserInput += '1';
+                          setState(() {});
+                        },
                       ),
-                      MYButton(title: '2', onPress: () {}),
-                      MYButton(title: '3', onPress: () {}),
+                      MYButton(
+                          title: '2',
+                          onPress: () {
+                            UserInput += '2';
+                            setState(() {});
+                          }),
+                      MYButton(
+                          title: '3',
+                          onPress: () {
+                            UserInput += '3';
+                            setState(() {});
+                          }),
                       MYButton(
                         title: '+',
-                        onPress: () {},
+                        onPress: () {
+                          UserInput += '+';
+
+                          setState(() {});
+                        },
                         color: const Color.fromARGB(255, 1, 56, 153),
                       ),
                     ],
@@ -115,13 +179,31 @@ class _HomeScrState extends State<HomeScr> {
                     children: [
                       MYButton(
                         title: '0',
-                        onPress: () {},
+                        onPress: () {
+                          UserInput += '0';
+                          setState(() {});
+                        },
                       ),
-                      MYButton(title: '_', onPress: () {}),
-                      MYButton(title: 'DEL', onPress: () {}),
+                      MYButton(
+                          title: '.',
+                          onPress: () {
+                            UserInput += '.';
+                            setState(() {});
+                          }),
+                      MYButton(
+                          title: 'DEL',
+                          onPress: () {
+                            UserInput =
+                                UserInput.substring(0, UserInput.length - 1);
+                            setState(() {});
+                          }),
                       MYButton(
                         title: '=',
-                        onPress: () {},
+                        onPress: () {
+                          setState(() {
+                            equalPres();
+                          });
+                        },
                         color: const Color.fromARGB(255, 1, 56, 153),
                       ),
                     ],
@@ -133,5 +215,17 @@ class _HomeScrState extends State<HomeScr> {
         ),
       ),
     );
+  }
+
+  void equalPres() {
+  
+    String finalUserInput = UserInput;
+    finalUserInput = UserInput.replaceAll('x', '*');
+    Parser p = Parser();
+    Expression expression = p.parse(finalUserInput);
+    ContextModel contextModel = ContextModel();
+
+    double eval = expression.evaluate(EvaluationType.REAL, contextModel);
+    answer = eval.toString();
   }
 }
